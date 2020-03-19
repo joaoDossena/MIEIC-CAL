bd6d1bb66de315eca76f9b683aab82ac388144d4
joaoDossena <joaopdossena@gmail.com>
2020-03-19 17:26:43 +0000

    TP4 halfway done

---
+++ TP4 - Graphs: Representation, Breadth-first and Depth-first Search and Topological Sort/build/lib/googletest-master/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/joao/Desktop/FEUP/CAL/TP4 - Graphs: Representation, Breadth-first and Depth-first Search and Topological Sort/lib/googletest-master/googlemock
+# Build directory: /home/joao/Desktop/FEUP/CAL/TP4 - Graphs: Representation, Breadth-first and Depth-first Search and Topological Sort/build/lib/googletest-master/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
